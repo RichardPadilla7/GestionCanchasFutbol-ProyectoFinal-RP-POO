401205977173e5a1a8f4871a7a57b1ffd3a97cdb
RichardPadilla7 <richardpadilla3rod@gmail.com>
2024-07-23 21:39:09 -0500

    boton inicar sesion prt 1

--- src/LoginFutbol.java
+++ src/LoginFutbol.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.*;
 
 public class LoginFutbol {
     public JPanel login;
@@ -20,6 +20,12 @@ public class LoginFutbol {
     public LoginFutbol(JFrame frame) {
         this.LoginFrame = frame;
 
+        //Opciones del JComboBox pára el modo de registro
+        modosbtn.addItem("Administrador");
+        modosbtn.addItem("Jugador");
+        modosbtn.addItem("Encargado");
+
+
         bntregistrar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -36,12 +42,60 @@ public class LoginFutbol {
         });
 
 
-
-
         btnsesion.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
+                String email = usertext.getText();
+                String contrasenia = new String(contratext.getPassword());
+
+                // Conectar a la base de datos
+                String url = "jdbc:mysql://localhost:3306/reservasCanchas";
+                String user = "root";
+                String password = "123456";
+
+                // Validar los datos del usuario en la base de datos
+                try (Connection conn = DriverManager.getConnection(url, user, password)) {
+                    String sql = "SELECT modo FROM usuarios WHERE email = ? AND contrasenia = ?";
+                    PreparedStatement pstmt = conn.prepareStatement(sql);
+                    pstmt.setString(1, email);
+                    pstmt.setString(2, contrasenia);
+
+                    ResultSet resultSet = pstmt.executeQuery();
+
+                    if (resultSet.next()) {
+                        String modo = resultSet.getString("modo");
+
+                        // Abrir la ventana dependiendo del modo de registro
+                        if (modo.equals("Administrador")) {
+                            LoginFrame.dispose();
+
+
+
+
+
+                        } else if (modo.equals("Jugador")) {
+                            LoginFrame.dispose();
+
+
+
+
+
+
+                        } else if (modo.equals("Encargado")) {
+                            LoginFrame.dispose();
+
+
+
+
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Usuario no encontrado o contraseña incorrecta");
+                        }
+                    }
+                    } catch (SQLException ex) {
+                    ex.printStackTrace();
+                    JOptionPane.showMessageDialog(null, "Error en la base de datos: " + ex.getMessage());
+                }
             }
         });
     }
