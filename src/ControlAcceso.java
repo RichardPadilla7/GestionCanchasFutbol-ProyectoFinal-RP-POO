e6a4e487f7f8ae50c9ff1e8d4c8bfea0f763fe45
RichardPadilla7 <richardpadilla3rod@gmail.com>
2024-08-04 12:19:15 -0500

    corriguiendo errores

--- src/ControlAcceso.java
+++ src/ControlAcceso.java
@@ -4,13 +4,13 @@ import java.awt.event.ActionListener;
 import java.sql.*;
 
 public class ControlAcceso {
-    public JTextArea textArea1;
+    public JTextArea mostrarInfo;
     public JTextField cedulaText;
     public JTextField correoText;
     public JButton autorizarButton;
     public JButton regresarButton;
     public JLabel img;
-    public JLabel mostrarInfo;
+    public JLabel titulo;
     public JLabel cedula;
     public JLabel correoRegistrado;
     public JPanel controlar;
@@ -20,7 +20,6 @@ public class ControlAcceso {
     public ControlAcceso(JFrame controlframe, JFrame encargadoframe) {
         this.controlFrame = controlframe;
         this.encargadoFrame = encargadoframe;
-
 
         autorizarButton.addActionListener(new ActionListener() {
             @Override
@@ -34,48 +33,48 @@ public class ControlAcceso {
                 String correo = correoText.getText();
 
                 if (cedula.isEmpty() || correo.isEmpty()) {
-                    JOptionPane.showMessageDialog(controlFrame, "Ingrese cedula y correo electronico.");
+                    JOptionPane.showMessageDialog(controlFrame, "Ingrese cédula y correo electrónico.");
                     return;
                 }
 
                 try (Connection conn = DriverManager.getConnection(url, user, password)) {
+
+                    // Verificar si el usuario está registrado
                     String sql = "SELECT * FROM usuarios WHERE cedula = ? AND email = ?";
                     PreparedStatement pstmt = conn.prepareStatement(sql);
                     pstmt.setString(1, cedula);
                     pstmt.setString(2, correo);
 
                     ResultSet resultSet = pstmt.executeQuery();
+                    StringBuilder info = new StringBuilder();
 
                     if (resultSet.next()) {
-                        // Obtener los detalles del usuario
-                        String nombre = resultSet.getString("nombre");
-                        StringBuilder info = new StringBuilder();
-                        info.append("Bienvenido, ").append(nombre).append("\n");
-
-                        // Obtener los detalles de la reserva
+                        // Usuario registrado, verificar reservas
                         String sqlReserva = "SELECT * FROM reservar_canchas WHERE cedula = ?";
                         PreparedStatement pstmtReserva = conn.prepareStatement(sqlReserva);
                         pstmtReserva.setString(1, cedula);
 
                         ResultSet rsReserva = pstmtReserva.executeQuery();
+
                         if (rsReserva.next()) {
-                            info.append("Detalles de la Reserva:\n");
-                            info.append("\nFecha: ").append(rsReserva.getDate("fecha")).append("\n");
-                            info.append("\nHora Inicio: ").append(rsReserva.getTime("hora")).append("\n");
-                            info.append("\nHora Fin: ").append(rsReserva.getTime("hora_fin")).append("\n");
-                            info.append("\nTipo de Cancha: ").append(rsReserva.getString("tipoCanchas_Reservas")).append("\n");
-                            info.append("\nDisfrute su partido!.");
+                            info.append(" -- Detalles de la Reserva --\n");
+                            info.append("Fecha: ").append(rsReserva.getDate("fecha")).append("\n");
+                            info.append("Hora Inicio: ").append(rsReserva.getTime("hora")).append("\n");
+                            info.append("Hora Fin: ").append(rsReserva.getTime("hora_fin")).append("\n");
+                            info.append("Tipo de Cancha: ").append(rsReserva.getString("tipoCanchas_Reservas")).append("\n\n");
+                            info.append("¡Usted esta autorizado, Disfrute su partido!");
                         } else {
-                            info.append("\nNo tiene reservas registradas.");
+                            info.append("No tiene reservas registradas.");
                         }
+
                         mostrarInfo.setText(info.toString());
                         cedulaText.setEditable(false);
                         correoText.setEditable(false);
                         autorizarButton.setEnabled(false);
                         regresarButton.setEnabled(true);
-                    } else {
-                        JOptionPane.showMessageDialog(controlFrame, "Cedula o correo electronico incorrectos. No esta autorizado, registrese en el sistema de manera correcta.");
 
+                    } else {
+                        JOptionPane.showMessageDialog(controlFrame, "No esta autorizado. Registrese y Reserve la Cancha en el sistema de manera correcta.");
                     }
                 } catch (SQLException ex) {
                     ex.printStackTrace();
@@ -84,13 +83,12 @@ public class ControlAcceso {
             }
         });
 
-
         regresarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 controlFrame.dispose();
-                if (encargadoframe != null && !encargadoframe.isVisible()) {
-                    encargadoframe.setVisible(true);
+                if (encargadoFrame != null && !encargadoFrame.isVisible()) {
+                    encargadoFrame.setVisible(true);
                 }
             }
         });
